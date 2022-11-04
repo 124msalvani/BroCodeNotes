8688c00b9923cfbe9f15e8713afd26da497850e5
Margaret <124msalvani@frhsd.com>
2022-11-04 07:51:21 -0400

    Done

--- script2.js
+++ script2.js
@@ -1,5 +1,17 @@
 //Type Conversion = change the datatpe of a value to another (strngs, numbers, booleans)
-
+/*
 let age = window.prompt("How old are you?");
+console.log(typeof age);
+age = Number(age);
 age += 1;
 console.log("Happy Birthday! You are ", age, "years old!" );
+*/
+let x;
+let y;
+let z;
+x= Number("3.14");
+y= String(3.14);
+z= Boolean("")
+console.log (x, typeof x);
+console.log (y, typeof y);
+console.log (z, typeof z);
